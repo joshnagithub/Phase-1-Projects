675d19ed6c4103f3a746a3b5f9d44acf7e6b6c47
joshnagithub <joshnasreenivasulu@gmail.com>
2022-02-01 17:21:05 +0530

    phase-1

---
+++ Java Programs/src/Phase1/ValidationoOfEmail.java
@@ -0,0 +1,40 @@
+package Phase1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidationoOfEmail {
+
+	public static boolean isValidEmail(String email) {
+		String regex = "^(.+)@(.+)$";
+		//initialize the pattern project
+		Pattern pattern = Pattern.compile(regex);
+	    Matcher matcher = pattern.matcher(email);
+	    return matcher.matches();
+	}
+	
+	public static void main(String[] args) {
+		List<String> emails = new ArrayList<String>();
+		//valid email addresses
+		emails.add("joshna@gmail.com");
+		emails.add("sreenivasulu@gmail.com");
+		emails.add("vennela@gmail.com");
+		//invalid email addresses
+		emails.add("prabhas gmail.com");
+		emails.add("jahnavi$gmail.com");
+		emails.add("ajay#gmail.com");
+		
+		for (String value : emails) {
+			System.out.println("The Email address " + value + " is " + (isValidEmail(value) ? "valid" : "invalid"));
+		}
+		
+		System.out.println("Enter any email address to check");
+	    Scanner sc = new Scanner(System.in);
+	    String input = sc.nextLine();
+	    System.out.println("The Email address " + input + " is " + (isValidEmail(input) ? "valid" : "invalid"));
+	}
+
+}
